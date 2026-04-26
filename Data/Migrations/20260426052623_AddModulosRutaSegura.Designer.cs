6443367ed0c37a208cee908ebf6d3970fc20d968
Antony Veliz <antonyvelizgonzales@gmail.com>
2026-04-26 00:34:51 -0500

    confirmar reportes

--- Data/Migrations/20260426052623_AddModulosRutaSegura.Designer.cs
+++ Data/Migrations/20260426052623_AddModulosRutaSegura.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RutaSeguraMvc.Data;
 
@@ -10,9 +11,11 @@ using RutaSeguraMvc.Data;
 namespace RutaSeguraMvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260426052623_AddModulosRutaSegura")]
+    partial class AddModulosRutaSegura
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.26");
