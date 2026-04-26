5c9706919113825d7fcf1f0379da226fe9996c0b
Antony Veliz <antonyvelizgonzales@gmail.com>
2026-04-25 22:51:14 -0500

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RutaSeguraMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
